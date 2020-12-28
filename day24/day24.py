4b7b38dcd5d24390be99548805087af17b2b8b12
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2020-12-28 17:19:01 -0500

    Day 24 (part 2)

--- day24/day24.py
+++ day24/day24.py
@@ -15,10 +15,12 @@ Direction = Literal['ne', 'e', 'se', 'sw', 'w', 'nw']
 class MainArgs(Tap):
     file: pathlib.Path  # A text file with a list of directions
     size: int  # The size of the floor
+    cycles: int  # The number of cycles to use for part 2
 
     def configure(self):
         self.add_argument('file', type=pathlib.Path)
         self.add_argument('size')
+        self.add_argument('cycles')
 
 
 def main(args: MainArgs):
@@ -27,6 +29,25 @@ def main(args: MainArgs):
         directions = readDirections(file)
         origin, floor = initializeFloor(args.size, directions)
     print(np.count_nonzero(floor))
+    floor = run(floor, args.cycles)
+    print(np.count_nonzero(floor))
+
+
+def run(floor: np.ndarray, cycles: int) -> np.ndarray:
+    for _ in range(cycles):
+        actives = np.pad(np.where(floor, 1, 0), 1)
+        neighbors = np.sum([
+            actives[:-2, 2:],  # NE
+            actives[1:-1, 2:],  # E
+            actives[2:, 1:-1],  # SE
+            actives[2:, :-2],  # SW
+            actives[1:-1, :-2],  # W
+            actives[:-2, 1:-1],  # NW
+        ], 0)
+        update = np.where(np.logical_and(floor, np.logical_or(neighbors == 0, neighbors > 2)), 0, -1)
+        update = np.where(np.logical_and(~floor, neighbors == 2), 1, update)
+        floor = np.where(update != -1, update == 1, floor)
+    return floor
 
 
 def initializeFloor(size: int, directions: Iterable[Iterable[Direction]]) -> tuple[tuple[int, int], np.ndarray]:
